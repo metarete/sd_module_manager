c93fab0b4583da4f8133d0346e388be136258046
tode <c.todeschini@metarete.it>
2022-12-27 10:09:00 +0100

    fix(User): modificata migration per includere anche proprietà username

--- migrations/Version20221005072916.php
+++ migrations/Version20221005072916.php
@@ -20,12 +20,12 @@ final class Version20221005072916 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD name VARCHAR(255) NOT NULL, ADD surname VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD name VARCHAR(255) NOT NULL, ADD surname VARCHAR(255) NOT NULL, ADD username VARCHAR(180)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP name, DROP surname');
+        $this->addSql('ALTER TABLE user DROP name, DROP surname, DROP username');
     }
 }
